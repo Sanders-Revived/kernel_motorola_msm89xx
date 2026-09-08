77e6722bf46dbbed5546682ee85e5e929b8da8d3
Wiktor Strzębała <wiktorek140@tlen.pl>
2026-09-07 21:56:42 -0300

    sound: montana: add CS35L35 support
    Combines the codec and MSM8952 machine-driver integration.

--- techpack/audio/asoc/msm8952.c
+++ techpack/audio/asoc/msm8952.c
@@ -71,6 +71,15 @@ static atomic_t quin_mi2s_clk_ref;
 static atomic_t auxpcm_mi2s_clk_ref;
 static struct snd_info_entry *codec_root;
 
+#ifdef CONFIG_SND_SOC_CS35L35
+	struct mutex l35_mclk_mutex;
+	static atomic_t l35_mclk_rsc_ref;
+#ifdef CONFIG_SND_CS35L35_OSR_CLK
+	static struct clk *codec_clk;
+	static struct platform_device *spdev;
+#endif
+#endif
+
 static int msm8952_enable_dig_cdc_clk(struct snd_soc_codec *codec, int enable,
 					bool dapm);
 static bool msm8952_swap_gnd_mic(struct snd_soc_codec *codec, bool active);
@@ -134,6 +143,17 @@ static struct afe_clk_set wsa_ana_clk = {
 	Q6AFE_LPASS_CLK_ROOT_DEFAULT,
 	0,
 };
+
+#ifdef CONFIG_SND_SOC_CS35L35
+static struct afe_clk_set l35_ana_clk = {
+	AFE_API_VERSION_I2S_CONFIG,
+	Q6AFE_LPASS_CLK_ID_MCLK_2,
+	Q6AFE_LPASS_OSR_CLK_12_P288_MHZ,
+	Q6AFE_LPASS_CLK_ATTRIBUTE_COUPLE_NO,
+	Q6AFE_LPASS_CLK_ROOT_DEFAULT,
+	0,
+};
+#endif
 
 static char const *rx_bit_format_text[] = {"S16_LE", "S24_LE", "S24_3LE"};
 static const char *const mi2s_ch_text[] = {"One", "Two"};
@@ -1464,6 +1484,52 @@ static void msm_quat_mi2s_snd_shutdown(struct snd_pcm_substream *substream)
 	}
 }
 
+#ifdef CONFIG_SND_SOC_CS35L35
+static int msm8952_enable_cs35l35_mclk(struct snd_soc_card *card, bool enable)
+{
+	int ret = 0;
+
+	mutex_lock(&l35_mclk_mutex);
+	if (enable) {
+		if (!atomic_read(&l35_mclk_rsc_ref)) {
+			pr_debug("%s: going to enable afe clock for cs35l35\n",
+				__func__);
+			l35_ana_clk.enable = true;
+			l35_ana_clk.clk_freq_in_hz = Q6AFE_LPASS_OSR_CLK_12_P288_MHZ;
+			ret = afe_set_lpass_clock_v2(
+					AFE_PORT_ID_SECONDARY_MI2S_RX,
+					&l35_ana_clk);
+			if (ret < 0) {
+				pr_err("%s: failed to enable mclk %d\n",
+					__func__, ret);
+				goto done;
+			}
+		}
+		atomic_inc_return(&l35_mclk_rsc_ref);
+	} else {
+		if (!atomic_read(&l35_mclk_rsc_ref))
+			goto done;
+		if (!atomic_dec_return(&l35_mclk_rsc_ref)) {
+			pr_debug("%s: going to disable afe clock for cs35l35\n",
+				__func__);
+			l35_ana_clk.enable = false;
+			ret = afe_set_lpass_clock_v2(
+					AFE_PORT_ID_SECONDARY_MI2S_RX,
+					&l35_ana_clk);
+			if (ret < 0) {
+				pr_err("%s: failed to disable mclk %d\n",
+					__func__, ret);
+				goto done;
+			}
+		}
+	}
+
+done:
+	mutex_unlock(&l35_mclk_mutex);
+	return ret;
+}
+#endif
+
 static int msm_quin_mi2s_snd_startup(struct snd_pcm_substream *substream)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
@@ -1493,6 +1559,22 @@ static int msm_quin_mi2s_snd_startup(struct snd_pcm_substream *substream)
 		pr_err("failed to enable sclk\n");
 		return ret;
 	}
+#ifdef CONFIG_SND_SOC_CS35L35
+	ret = msm_cdc_pinctrl_select_active_state(pdata->mi2s_gpio_p[CS35L35]);
+	if (ret < 0) {
+		pr_err("failed to enable codec gpios, cs35l35_mclk\n");
+		goto err;
+	}
+	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
+		pr_debug("%s, going to enable cs35l35_mclk\n", __func__);
+		ret = msm8952_enable_cs35l35_mclk(card, true);
+		if (ret < 0) {
+			pr_err("%s: failed to enable mclk for cs35l35 %d\n",
+				__func__, ret);
+			return ret;
+		}
+	}
+#endif
 	if (pdata->mi2s_gpio_p[QUIN_MI2S]) {
 		ret =  msm_cdc_pinctrl_select_active_state(
 			pdata->mi2s_gpio_p[QUIN_MI2S]);
@@ -1538,6 +1620,24 @@ static void msm_quin_mi2s_snd_shutdown(struct snd_pcm_substream *substream)
 			return;
 		}
 	}
+#ifdef CONFIG_SND_SOC_CS35L35
+	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
+		pr_debug("%s, going to disable cs35l35 mclk\n", __func__);
+		ret = msm8952_enable_cs35l35_mclk(card, false);
+		if (ret < 0) {
+			pr_err("%s: failed to disable mclk for l35 %d\n",
+				__func__, ret);
+			return;
+		}
+	}
+	pr_debug("%s, going to de-activate cs35l35_clk\n", __func__);
+	ret = msm_cdc_pinctrl_select_sleep_state(pdata->mi2s_gpio_p[CS35L35]);
+	if (ret < 0) {
+		pr_err("%s: gpio set cannot be de-activated %s",
+				__func__, "cs35l35_mclk");
+		return;
+	}
+#endif
 }
 
 static void *def_msm8952_wcd_mbhc_cal(void)
@@ -1683,6 +1783,40 @@ static struct snd_soc_ops msm_pri_auxpcm_be_ops = {
 	.startup = msm_prim_auxpcm_startup,
 	.shutdown = msm_prim_auxpcm_shutdown,
 };
+
+#ifdef CONFIG_SND_SOC_CS35L35
+static int cs35l35_dai_init(struct snd_soc_pcm_runtime *rtd)
+{
+	int ret;
+	struct snd_soc_dai *codec_dai = rtd->codec_dai;
+	struct snd_soc_codec *codec = codec_dai->codec;
+	struct snd_soc_dapm_context *dapm = snd_soc_codec_get_dapm(codec);
+
+#ifdef CONFIG_SND_CS35L35_OSR_CLK
+	codec_clk = clk_get(&spdev->dev, "osr_clk");
+	if (IS_ERR(codec_clk)) {
+		dev_err(rtd->cpu_dai->dev,
+			"florida_dai_init: can't create the clock!!\n");
+	} else {
+		clk_set_rate(codec_clk, 9600000);
+	}
+#endif
+	ret = snd_soc_codec_set_sysclk(codec, 0, 0,
+					Q6AFE_LPASS_OSR_CLK_12_P288_MHZ,
+					SND_SOC_CLOCK_IN);
+	if (ret < 0)
+		pr_err("%s: set sysclk failed, err:%d\n", __func__, ret);
+	ret = snd_soc_dai_set_sysclk(codec_dai, 0,
+					Q6AFE_LPASS_IBIT_CLK_1_P536_MHZ,
+					SND_SOC_CLOCK_IN);
+	if (ret < 0)
+		pr_err("%s: set dai_sysclk failed, err:%d\n", __func__, ret);
+	snd_soc_dapm_ignore_suspend(dapm, "AMP Playback");
+	snd_soc_dapm_ignore_suspend(dapm, "AMP Capture");
+	snd_soc_dapm_sync(dapm);
+	return ret;
+}
+#endif
 
 struct snd_soc_dai_link_component dlc_rx1[] = {
 	{
@@ -2886,6 +3020,26 @@ static struct snd_soc_dai_link msm8952_aw882xx_quin_dai_link[] = {
 };
 
 static struct snd_soc_dai_link msm8952_quin_dai_link[] = {
+#if defined(CONFIG_SND_SOC_CS35L35) && !defined(CONFIG_SND_CS35L35_QUAT_I2S)
+	{
+		.name = LPASS_BE_QUIN_MI2S_RX,
+		.stream_name = "Quinary MI2S Playback",
+		.cpu_dai_name = "msm-dai-q6-mi2s.4",
+		.platform_name = "msm-pcm-routing",
+		.codec_name = "cs35l35.7-0040",
+		.codec_dai_name = "cs35l35-pcm",
+		.init = cs35l35_dai_init,
+		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
+				SND_SOC_DAIFMT_CBS_CFS,
+		.dpcm_playback = 1,
+		.no_pcm = 1,
+		.id = MSM_BACKEND_DAI_QUINARY_MI2S_RX,
+		.be_hw_params_fixup = msm_mi2s_rx_be_hw_params_fixup,
+		.ops = &msm8952_quin_mi2s_be_ops,
+		.ignore_pmdown_time = 1, /* dai link has playback support */
+		.ignore_suspend = 1,
+	},
+#else
 	{
 		.name = LPASS_BE_QUIN_MI2S_RX,
 		.stream_name = "Quinary MI2S Playback",
@@ -2901,6 +3055,7 @@ static struct snd_soc_dai_link msm8952_quin_dai_link[] = {
 		.ignore_pmdown_time = 1, /* dai link has playback support */
 		.ignore_suspend = 1,
 	},
+#endif
 };
 
 static struct snd_soc_dai_link msm8952_split_a2dp_dai_link[] = {
@@ -3532,6 +3687,10 @@ parse_mclk_freq:
 					"qcom,quat-mi2s-gpios", 0);
 	pdata->mi2s_gpio_p[QUIN_MI2S] = of_parse_phandle(pdev->dev.of_node,
 					"qcom,quin-mi2s-gpios", 0);
+#ifdef CONFIG_SND_SOC_CS35L35
+	pdata->mi2s_gpio_p[CS35L35] = of_parse_phandle(pdev->dev.of_node,
+					"qcom,cs35l35-gpios", 0);
+#endif
 
 	ret = of_property_read_string(pdev->dev.of_node,
 		hs_micbias_type, &type);
@@ -3595,6 +3754,10 @@ parse_mclk_freq:
 	atomic_set(&quat_mi2s_clk_ref, 0);
 	atomic_set(&quin_mi2s_clk_ref, 0);
 	atomic_set(&auxpcm_mi2s_clk_ref, 0);
+#ifdef CONFIG_SND_SOC_CS35L35
+	mutex_init(&l35_mclk_mutex);
+	atomic_set(&l35_mclk_rsc_ref, 0);
+#endif
 
 	ret = snd_soc_of_parse_audio_routing(card,
 			"qcom,audio-routing");
